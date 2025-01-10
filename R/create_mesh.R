e0cb4843083d27d0c3c2ba2f8bec352930b3a471
Sz-Tim <tim.szewczyk@gmail.com>
2025-01-10 12:30:43 +0000

    new functions for creating mesh files from a .nc, and for downloading from thredds

---
+++ R/create_mesh.R
@@ -0,0 +1,133 @@
+
+#' Create Mesh Files from NetCDF File
+#'
+#' This function creates mesh files from a NetCDF file. Note that there is a two
+#' stage process where only a *_TEMP.gpkg file is output first, allowing the
+#' manual identification of open boundary elements which are then saved to a
+#' .csv and provided to the `open_elems_csv` argument
+#'
+#' @param nc_file Character. The path to the NetCDF file.
+#' @param open_elems_csv Character. The path to the CSV file containing open boundary elements. Default is NULL.
+#' @param out_dir Character. The output directory where the mesh files will be saved.
+#' @param domain Character. The domain name for the mesh.
+#'
+#' @return Character. A message indicating the status of the mesh creation.
+#' @export
+#'
+#' @examples
+#' create_mesh_from_nc(nc_file="path/to/nc_file.nc", open_elems_csv="path/to/open_elems.csv", out_dir="~/output", domain="WeStCOMS2")
+create_mesh_files_from_nc <- function(nc_file, open_elems_csv, out_dir, domain) {
+
+  library(tidyverse); library(ncdf4); library(sf); library(glue)
+
+  day2 <- nc_open(nc_file)
+  if(!is.null(open_elems_csv)) {
+    open_elems <- read_csv(open_elems_csv)
+  }
+
+
+  # extract dims ------------------------------------------------------------
+
+  # Variables to extract, following Tom's naming convention for clarity (see
+  # WeStCOMS2_mesh.nc) and to avoid rewriting a bunch of Java code
+  nc <- list(
+    uvnode=cbind(ncvar_get(day2, "lonc"), ncvar_get(day2, "latc")),
+    nodexy=cbind(ncvar_get(day2, "lon"), ncvar_get(day2, "lat")),
+    trinodes=ncvar_get(day2, "nv"),
+    nbe=ncvar_get(day2, "nbe"),
+    depthNodexy=ncvar_get(day2, "h"),
+    depthUvnode=ncvar_get(day2, "h_center"),
+    siglay=ncvar_get(day2, "siglay")[1,],
+    siglev=ncvar_get(day2, "siglev")[1,]
+  )
+  nc_close(day2)
+  nc$boundaryNodesAll <- which(rowSums(nc$nbe==0) > 0)
+  if(!is.null(open_elems_csv)) {
+    nc$boundaryNodesOpen <- sort(unique(unlist(open_elems %>% select(starts_with("trinode")))))
+  }
+  nc$uvnode_os <- as_tibble(nc$uvnode) %>%
+    st_as_sf(coords=c("V1", "V2"), crs=4326) %>%
+    st_transform(27700) %>% st_coordinates
+  nc$nodexy_os <- as_tibble(nc$nodexy) %>%
+    st_as_sf(coords=c("V1", "V2"), crs=4326) %>%
+    st_transform(27700) %>% st_coordinates
+
+
+  # assign dims -------------------------------------------------------------
+
+  elems <- ncdim_def("elems", "element", 1:nrow(nc$uvnode))
+  two <- ncdim_def("two", "two", 1:2)
+  node <- ncdim_def("node", "nodes", 1:nrow(nc$nodexy))
+  nele <- elems
+  three <- ncdim_def("three", "three", 1:3)
+  siglayers <- ncdim_def("siglayers", "sigma layers", 1:length(nc$siglay))
+  siglevels <- ncdim_def("siglevels", "sigma levels", 1:length(nc$siglev))
+  bnode <- ncdim_def("bnode", "boundary", 1:length(nc$boundaryNodesAll))
+  if(!is.null(open_elems_csv)) {
+    obcnode <- ncdim_def("obcnode", "boundary", 1:length(nc$boundaryNodesOpen))
+  }
+
+
+
+  # create vars -------------------------------------------------------------
+
+  mesh.vars <- list(
+    uvnode=ncvar_def("uvnode", "degrees", list(elems, two)),
+    nodexy=ncvar_def("nodexy", "degrees", list(node, two)),
+    uvnode_os=ncvar_def("uvnode_os", "m", list(elems, two)),
+    nodexy_os=ncvar_def("nodexy_os", "m", list(node, two)),
+    trinodes=ncvar_def("trinodes", "", list(nele, three), prec="integer"),
+    nbe=ncvar_def("nbe", "", list(nele, three), prec="integer"),
+    depthUvnode=ncvar_def("depthUvnode", "m", elems),
+    depthNodexy=ncvar_def("depthNodexy", "m", node),
+    boundaryNodesAll=ncvar_def("boundaryNodesAll", "", bnode, prec="integer"),
+    siglay=ncvar_def("siglay", "proportion", siglayers),
+    siglev=ncvar_def("siglev", "proportion", siglevels)
+  )
+  if(!is.null(open_elems_csv)) {
+    mesh.vars$boundaryNodesOpen=ncvar_def("boundaryNodesOpen", "", obcnode, prec="integer")
+  }
+
+
+
+  # create mesh.nc ----------------------------------------------------------
+
+  if(!is.null(open_elems_csv)) {
+    mesh.nc <- nc_create(glue("{out.dir}/{domain}_mesh.nc"), mesh.vars)
+    iwalk(names(mesh.vars), ~ncvar_put(mesh.nc, .x, nc[[.x]]))
+    nc_close(mesh.nc)
+  }
+
+
+
+  # create mesh.gpkg --------------------------------------------------------
+
+  suffix <- ifelse(is.null(open_elems_csv), "_TEMP", "")
+  mesh.sf <- tibble(i=1:nrow(nc$uvnode_os),
+                    depth=nc$depthUvnode,
+                    lonc=nc$uvnode_os[,1],
+                    latc=nc$uvnode_os[,2],
+                    trinode_1=nc$trinodes[,1],
+                    trinode_2=nc$trinodes[,2],
+                    trinode_3=nc$trinodes[,3]) %>%
+    rowwise() %>%
+    mutate(coords=list(nc$nodexy_os[c(trinode_1, trinode_2, trinode_3, trinode_1),]),
+           geom=list(st_polygon(list(coords)))) %>%
+    ungroup %>%
+    st_as_sf(crs=27700) %>%
+    mutate(area=st_area(.)) %>%
+    select(i, area, depth, trinode_1, trinode_2, trinode_3, geom)
+  write_sf(mesh.sf, glue("{out.dir}/{domain}_mesh{suffix}.gpkg"))
+
+  if(!is.null(open_elems_csv)) {
+    mesh.footprint <- mesh.sf %>% st_union
+    write_sf(mesh.footprint, glue("{out.dir}/{domain}_mesh_footprint.gpkg"))
+  }
+
+  if(is.null(open_elems_csv)) {
+    return(glue("Please open {out.dir}/{domain}_mesh{suffix}.gpkg and identify open boundary elements."))
+  } else {
+    return("Created {domain}_mesh.gpkg, {domain}_mesh_footprint.gpkg, and {domain}_mesh.nc in {out.dir}")
+  }
+
+}
---
+++ R/download.R
@@ -0,0 +1,93 @@
+
+
+#' Download NetCDF Files from THREDDS Server
+#'
+#' This function downloads NetCDF files from a specified THREDDS server catalog for a given domain and year.
+#'
+#' @param domain Character. The domain to download data from. Options are "westcoms2" or "etive28". Default is "westcoms2".
+#' @param year Numeric. The year of the data to download. Default is 2024.
+#' @param out_dir Character. The output directory where the downloaded files will be saved. Default is "~/".
+#' @param cores Numeric. The number of cores to use for parallel processing. Default is 4.
+#' @param overwrite Logical. Whether to overwrite existing files. Default is FALSE.
+#' @param start_ymd Date. The start date for the data to download. Default is NULL, which sets it to January 1st of the specified year.
+#' @param end_ymd Date. The end date for the data to download. Default is NULL, which sets it to December 31st of the specified year.
+#'
+#' @return None. The function downloads files to the specified output directory.
+#' @export
+#'
+#' @examples
+#' download_from_thredds(domain="westcoms2", year=2024, out_dir="~/data", cores=4, overwrite=FALSE, start_ymd=NULL, end_ymd=NULL)
+download_from_thredds <- function(domain="westcoms2",
+                                  year=2024,
+                                  out_dir="~/",
+                                  cores=4,
+                                  overwrite=FALSE,
+                                  start_ymd=NULL,
+                                  end_ymd=NULL) {
+  library(tidyverse); library(ncdf4); library(lubridate); library(glue);
+  library(xml2); library(rvest)
+  library(doFuture); library(progressr)
+
+  domain_name <- switch(domain,
+                        westcoms2="WeStCOMS2",
+                        etive28="etive28")
+  catalog <- glue("https://thredds.sams.ac.uk/thredds/catalog/scoats-{domain}/Archive/netcdf_{year}/catalog.html")
+  file_base <- glue("https://thredds.sams.ac.uk/thredds/fileServer/scoats-{domain}/Archive/netcdf_{year}/")
+  out_dir <- glue("{out_dir}/{domain_name}/netcdf_{year}/")
+  dir.create(out_dir, showWarnings=F, recursive=T)
+
+  if(is.null(start_ymd)) {
+    start_ymd <- ymd(glue("{year}-01-01"))
+  }
+  if(is.null(end_ymd)) {
+    end_ymd <- ymd(glue("{year}-12-31"))
+  }
+
+  dates <- seq(start_ymd, end_ymd, by=1) |> format("%Y%m%d")
+  if(!overwrite) {
+    f <- str_split_fixed(dir(out_dir, "*.nc", recursive=T), "_", 4)[,3]
+    dates <- dates[! dates %in% f]
+  }
+
+  westcom_links <- read_html(catalog) |> html_nodes("a") |> html_attr("href")
+  nc_links <- map(dates, ~grep(.x, westcom_links, value=T) |> basename()) |>
+    do.call('c', args=_)
+
+  plan(multisession, workers=ncores)
+  handlers(global=T)
+  download_nc(nc_links)
+  plan(sequential)
+}
+
+
+
+
+
+#' Download NetCDF Files
+#'
+#' This function downloads NetCDF files from a specified list of links.
+#'
+#' @param nc_links Character vector. A list of NetCDF file links to download.
+#' @param file_base Character. The base URL for the files to be downloaded.
+#' @param out_dir Character. The output directory where the downloaded files will be saved.
+#'
+#' @return None. The function downloads files to the specified output directory.
+#' @export
+#'
+#' @examples
+#' download_nc(nc_links=c("file1.nc", "file2.nc"), file_base="https://example.com/files/", out_dir="~/data/")
+download_nc <- function(nc_links, file_base, out_dir) {
+  library(tidyverse); library(ncdf4); library(lubridate); library(glue);
+  library(xml2); library(rvest)
+  library(doFuture); library(progressr)
+  p <- progressor(along=nc_links)
+  foreach(i=nc_links,
+          .options.future=list(globals=structure(TRUE, add=c("file_base", "out_dir")))
+  ) %dofuture% {
+    options(timeout=3600*5)
+    if( !file.exists(glue("{out_dir}{i}")) ) {
+      download.file(glue("{file_base}{i}"), glue("{out_dir}{i}"), method="wget")
+    }
+    p(sprintf("i=%s", i))
+  }
+}
